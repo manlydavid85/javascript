e50b226f9573c2a7279e106d3f03a549a0628914
David Manly <D.manly@revel-health.com>
2019-11-17 17:25:56 -0600

    new assignment

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
-var button = document.getElementById('clickButton');
-button.addEventListener('click', function(params) {
+var englishButton = document.getElementById('englishButton');
+console.log(englishButton)
+englishButton.addEventListener('click', function(params) {
     alert("Good Morning!");
-})
+});
 
-var button = document.getElementById('clickButton');
-button.addEventListener('click', function(params) {
-    alert("Good Morning!");
-})
+var spanishButton = document.getElementById('spanishButton');
+spanishButton.addEventListener('click', function(params) {
+    alert("Buaenos Dias!");
+});
+
+var chineseButton = document.getElementById('chineseButton');
+chineseButton.addEventListener('click', function(params) {
+    alert("Zǎoshang hǎo!");
+});
+
+var greekButton = document.getElementById('greekButton');
+greekButton.addEventListener('click', function(params) {
+    alert("Kaliméra!");
+});
+var expandibleButton = document.getElementById('expandOnClick');
+expandibleButton.addEventListener('click', function(params) {
+    this.classList.add("increase");
+
+});
